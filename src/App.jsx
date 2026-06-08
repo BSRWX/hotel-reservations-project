68491b0e0802f0c43e88e7cae554bde425ab5167
Kacper <142674290+BSRWX@users.noreply.github.com>
2026-06-08 23:37:59 +0200

    Add HashRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RoomSearch from './components/RoomSearch'
 import Home from "./components/Home";
 import Login from "./components/Login";
